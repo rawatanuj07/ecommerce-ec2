929be97e795073e121f4f638aaa12b634bb6ae17
rawatanuj07 <rawatanuj07@gmail.com>
2025-01-29 03:53:18 +0530

    mix

--- src/app/api/fetchh/route.ts
+++ src/app/api/fetchh/route.ts
@@ -65,7 +65,6 @@ export async function GET(req: NextRequest) {
     }
 
     const data = await response.json();
-    console.log(`Fetched ${type}:`, data); // Debugging information
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
