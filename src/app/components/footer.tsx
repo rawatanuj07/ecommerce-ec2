dac05a08ee4a4d912d41edaae64ee46ef8afdcdf
rawatanuj07 <rawatanuj07@gmail.com>
2025-02-06 18:33:23 +0530

    icons

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -8,30 +8,34 @@ import { MdOutlinePermContactCalendar } from "react-icons/md";
 export default function Footer() {
   return (
     <div className="sticky z-5 bottom-0 lg:hidden w-full flex justify-between items-center bg-white text-black h-16 border-t-2">
-      <div className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full">
-        <Link href="/">
-          <IoHome />
-          <span>Home</span>
-        </Link>
-      </div>
-      <div className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full">
-        <Link href="/register">
-          <FaUser />
-          <span>Register</span>
-        </Link>
-      </div>
-      <div className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full">
-        <Link href="/full">
-          <HiMiniShoppingBag />
-          <span>Products</span>
-        </Link>
-      </div>
-      <div className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full">
-        <Link href="/contact">
-          <MdOutlinePermContactCalendar />
-          <span>Contact-Us</span>
-        </Link>
-      </div>
+      <Link
+        href="/"
+        className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full"
+      >
+        <IoHome />
+        <span>Home</span>
+      </Link>
+      <Link
+        href="/register"
+        className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full"
+      >
+        <FaUser />
+        <span>Register</span>
+      </Link>
+      <Link
+        href="/full"
+        className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full"
+      >
+        <HiMiniShoppingBag />
+        <span>Products</span>
+      </Link>
+      <Link
+        href="/contact"
+        className="flex-1 flex flex-col justify-center items-center border border-gray-300 h-full"
+      >
+        <MdOutlinePermContactCalendar />
+        <span>Contact-Us</span>
+      </Link>
     </div>
   );
 }
